17bbbe4c3435edc8e007b75661213fe69fcad144
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-03-22 23:59:49 +0530

    Add project files.

---
+++ first .net/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace constructor
+{
+    class program
+    {
+        int i=10; 
+        int a=10;
+        public static void Main(string[] args)
+        {
+            string i;
+            int a;
+            Console.WriteLine("enter any number:");
+            i = (Console.ReadLine());
+            a= Convert.ToInt32(i);
+            if (a < 0)
+            {
+                Console.WriteLine("it is a negative number");
+            }
+            else
+            {
+                Console.WriteLine("it is a positive number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
